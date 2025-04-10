6b01ece7288cfb54c40906ee3332743c5394ff6c
Paula Frölander <paulashej@gmail.com>
2025-04-10 13:02:54 +0200

    made symfony website

---
+++ README.md
@@ -0,0 +1,16 @@
+# Website with Symfony
+
+A website for the MVC-course, made with object oriented PHP and the framework Symfony.
+
+![Screenshot of the website](public/img/readme_screenshot.png)
+
+## Prerequisites
+You have PHP, Composer and npm.
+
+## Get started
+git clone git@github.com:gitpau4/report-mvc.git
+cd report-mvc
+composer install
+npm install
+npm run build
+symfony server:start
---
+++ src/Controller/LuckyControllerJson.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyControllerJson
+{
+    #[Route("/api/quote")]
+    public function jsonQuote(): Response
+    {
+        $quotes = [
+            "Oroa dig inte för morgondagen, i övermorgon är den över. - Voltaire",
+            "Alla dessa dagar som kom och gick, inte visste jag att de var livet. - Stig Johansson",
+            "Samhället är människans verk - om något är fel kan vi ändra på det. - Olof Palme"
+        ];
+
+        $randNum = random_int(0, 2);
+        $randQuote = $quotes[$randNum];
+
+        date_default_timezone_set("Europe/Stockholm");
+        $dateToday = date("Y-m-d");
+
+        $timestamp = date("H:i:s");
+
+        $data = [
+            'citat' => $randQuote,
+            'dagens datum' => $dateToday,
+            'tidsstämpel' => $timestamp
+        ];
+
+        // $response = new Response();
+        // $response->setContent(json_encode($data));
+        // $response->headers->set('Content-Type', 'application/json');
+
+        // return $response;
+
+        // return new JsonResponse($data);
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
