ed9d237a5d62aad88e3c6734c32f268eed348577
Paula Frölander <paulashej@gmail.com>
2025-04-20 21:24:58 +0200

    Add controller and session debugging route

--- src/Card/DeckOfCards.php
+++ src/Card/DeckOfCards.php
@@ -7,8 +7,8 @@ use App\Card\Card;
 class DeckOfCards
 {
     private $deck = [];
-    private $values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    private $suits = ['spades', 'hearts', 'clubs', 'diamonds']
+    private $values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    private $suits = ['spades', 'hearts', 'clubs', 'diamonds'];
 
     public function __construct()
     {
