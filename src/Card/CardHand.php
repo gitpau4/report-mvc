48fa808996499fad5a507c4bd755b1a1ad879981
Paula Frölander <paulashej@gmail.com>
2025-04-20 12:40:31 +0200

    Add CardHand class

---
+++ src/Card/CardHand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Card;
+
+use App\Card\Card;
+
+class CardHand
+{
+    private $hand = [];
+
+    public function add(Card $card): void
+    {
+        $this->hand[] = $card;
+    }
+
+    public function getHand(): array
+    {
+        return $this->hand;
+    }
+
+    public function getNumberCards(): int
+    {
+        return count($this->hand);
+    }
+
+    public function getValues(): array
+    {
+        $values = [];
+        foreach ($this->hand as $card) {
+            $values[] = $card->getValue();
+        }
+        return $values;
+    }
+
+    public function getSuits(): array
+    {
+        $suits = [];
+        foreach ($this->hand as $card) {
+            $suits[] = $card->getSuit();
+        }
+        return $suits;
+    }
+}
