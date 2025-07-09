6c981276037305c362d1fbefcacf2deb74857e68
Paula Frölander <paulashej@gmail.com>
2025-07-09 15:52:52 +0200

    Add create new book route

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use App\Form\BookType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,6 +18,30 @@ final class LibraryController extends AbstractController
     {
         return $this->render('library/index.html.twig', [
             'controller_name' => 'LibraryController',
+        ]);
+    }
+
+    #[Route('/library/create', name: 'library_create')]
+    public function createBook(
+        Request $request,
+        ManagerRegistry $doctrine
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $book = new Book();
+
+        // skapar formulär enligt https://symfony.com/doc/current/forms.html
+        $form = $this->createForm(BookType::class, $book);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($book);
+            $entityManager->flush();
+
+            // return $this->redirectToRoute('book_index');
+        }
+
+        return $this->render('library/create.html.twig', [
+            'form' => $form
         ]);
     }
 }
