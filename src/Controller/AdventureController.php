d284b91dcf74ad6fe004c06438a0e930fe49a3ed
Paula Frölander <paulashej@gmail.com>
2025-11-09 17:06:46 +0100

    Add controller and twig templates for adventure game

--- src/Controller/AdventureController.php
+++ src/Controller/AdventureController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Adventure\AdventureLogic;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,5 +21,109 @@ class AdventureController extends AbstractController
     public function projAbout(): Response
     {
         return $this->render('proj/about.html.twig');
+    }
+    // kommer behöva lägga till img path i json filen
+
+    #[Route('/proj', name: 'adventure_post', methods: ['POST'])]
+    public function adventureCallback(
+        SessionInterface $session
+    ): Response {
+        $adventure = new AdventureLogic();
+        $session->set('adventure', $adventure);
+
+        return $this->redirectToRoute('adventure_play');
+    }
+
+    #[Route('/proj/play', name: 'adventure_play', methods: ['GET'])]
+    public function adventurePlay(
+        SessionInterface $session
+    ): Response {
+        $adventure = $session->get('adventure');
+        if (!$adventure) {
+            return $this->redirectToRoute('adventure');
+        }
+
+        $data = [
+            "currentRoom" => $adventure->getCurrentRoom(),
+            "isGameOver" => $adventure->getIsGameOver(),
+            "inventory" => $adventure->getInventoryAsString()
+        ];
+
+        return $this->render('proj/play.html.twig', $data);
+    }
+
+    #[Route('/proj/move', name: 'adventure_move', methods: ['POST'])]
+    public function adventureMove(
+        SessionInterface $session,
+        Request $request
+    ): Response {
+        $adventure = $session->get('adventure');
+        if (!$adventure) {
+            return $this->redirectToRoute('adventure');
+        }
+
+        $direction = $request->request->get('move');
+
+        $moveMsg = $adventure->move($direction);
+
+        // kanske göra egen styling för adventure flash, då byt ut notice till typ adventure, kanske två olika färger, gul och blå eller röd och grön
+        $this->addFlash(
+            'notice',
+            $moveMsg
+        );
+
+        $session->set('adventure', $adventure);
+
+        return $this->redirectToRoute('adventure_play');
+    }
+
+    #[Route('/proj/pick', name: 'adventure_pick', methods: ['POST'])]
+    public function adventurePick(
+        SessionInterface $session,
+        Request $request
+    ): Response {
+        $adventure = $session->get('adventure');
+        if (!$adventure) {
+            return $this->redirectToRoute('adventure');
+        }
+
+        $itemName = $request->request->get('pick');
+        $itemName = str_replace("Pick ", "", $itemName);
+
+        $pickMsg = $adventure->pickItem($itemName);
+
+        $this->addFlash(
+            'notice',
+            $pickMsg
+        );
+
+        $session->set('adventure', $adventure);
+
+        return $this->redirectToRoute('adventure_play');
+    }
+
+    #[Route('/proj/action', name: 'adventure_action', methods: ['POST'])]
+    public function adventureAction(
+        SessionInterface $session,
+        Request $request
+    ): Response {
+        $adventure = $session->get('adventure');
+        if (!$adventure) {
+            return $this->redirectToRoute('adventure');
+        }
+
+        $action = $request->request->get('action');
+        $action = str_replace("Interact with ", "", $action);
+
+        $actionMsg = $adventure->interact($action);
+
+        $this->addFlash(
+            'notice',
+            $actionMsg
+        );
+
+        $session->set('adventure', $adventure);
+
+        return $this->redirectToRoute('adventure_play');
     }
 }
